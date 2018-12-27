7c7ec4e3dd360c2d8767b0bd8bc6e590c83cb982
wuhao <hzwuhao8@gmail.com>
2018-12-27 17:41:49 +0800

    输出运行提示时 ，显示优化

--- runa.sh
+++ runa.sh
@@ -15,9 +15,15 @@ fi
 cat ~/Desktop/data/J${j}/j${j}.${index}.in   | python  j0${j}.py   > /tmp/a.txt ;
 
 if [ $flag =  "1" ]; then
+	echo "========================================"
+	echo "测试输入" 
 	cat  ~/Desktop/data/J${j}/j${j}.${index}.in ;
+	echo "标准结果"
 	cat ~/Desktop/data/J${j}/j${j}.${index}.out
+	echo "程序运行结果"
 	cat /tmp/a.txt ;
+	echo "========================================"
+
 fi
 
 diff /tmp/a.txt   ~/Desktop/data/J${j}/j${j}.${index}.out
