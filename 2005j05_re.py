0b22c25e6bc609255fe8b38c6dd293fac34f43c1
wuhao <wuhao@bokee.net>
2018-12-31 20:55:26 +0800

    使用 re ; 不停的替换 得到最简表达式

---
+++ 2005j05_re.py
@@ -0,0 +1,69 @@
+#use re
+
+import re
+def  myprint(x):
+    #print(x)
+    pass
+
+
+def my_reduce(s):
+
+    r2 = re.compile('A(NA)+')
+    r3 = re.compile('BAS')
+
+    for i in range(10):
+        s1 = r2.sub('A', s)
+        s2 = r3.sub('A', s1)
+        s = s2
+    return s
+
+
+def my_check(s):
+    myprint(s)
+    r0 = re.compile('A')
+    r1 = re.compile('ANA')
+    r2 = re.compile('A(NA)+')
+    r3 = re.compile('BAS')
+    s2 = my_reduce(s)
+    if r0.fullmatch(s2):
+        myprint(f"s2={s2}, r={r0}")
+        return True
+    elif r1.fullmatch(s2):
+        return True
+    elif r3.findall(s2):
+        return True
+    else:
+        return False
+    pass
+
+
+def myinput():
+    flag = True
+    res = []
+    while flag:
+        s = input()
+        if 'X' == s:
+            break
+        else:
+            res.append(s)
+
+    return res
+
+def main():
+    res = myinput()
+    for s  in res:
+        if my_check(s):
+          print('YES')
+        else:
+          print('NO')
+
+main()
+
+quit()
+
+assert my_check('A') , "A is valid"
+assert my_check('B') == False
+assert my_check('ANA')
+assert my_check('ANANA')
+assert my_check('BAS')
+assert my_check('BANANAS')
