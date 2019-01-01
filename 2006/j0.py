31d521ed2c78f5a03182295542d024e8b5b5b862
wuhao <wuhao@bokee.net>
2019-01-01 17:18:14 +0800

    j3
    j0 作为模版

---
+++ 2006/j0.py
@@ -0,0 +1,24 @@
+
+
+def my_print(x):
+    print(x)
+    pass
+
+
+def my_input():
+    pass
+
+
+def my_run(data):
+    pass
+
+
+def my_test():
+    pass
+
+
+def my_main():
+    my_test()
+
+
+my_main()
