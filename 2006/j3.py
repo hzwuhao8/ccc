1e7b47e351d3628460c5e46034b596c235255136
wuhao <wuhao@bokee.net>
2019-01-01 17:23:33 +0800

    add assert test

---
+++ 2006/j3.py
@@ -0,0 +1,31 @@
+
+
+def my_print(x):
+    print(x)
+    pass
+
+
+def my_input():
+    pass
+
+
+def my_run(data):
+    pass
+
+
+def my_test():
+    assert my_run('a') == 1
+    assert my_run('dada') == 4
+    assert my_run('bob') == 7
+    assert my_run('abba') == 12
+    assert my_run('cell') == 13
+    assert my_run('www') == 7
+
+    pass
+
+
+def my_main():
+    my_test()
+
+
+my_main()
