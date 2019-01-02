7cc7563a114005fc4dc218e41f8d70ef6f865204
wuhao <wuhao@bokee.net>
2019-01-02 19:40:38 +0800

    j2 init

---
+++ 2007/j2.py
@@ -0,0 +1,35 @@
+#
+
+
+def my_print(x, end="\n"):
+    print(x, end=end)
+    pass
+
+
+def my_input():
+    pass
+
+
+def my_run(data):
+
+    pass
+
+
+def my_unit_test():
+    pass
+
+
+def my_func_test():
+    assert my_run('CCC') == 'Canadian Computing Competition'
+    assert my_run('NOT EXIST') == 'NOT EXIST'
+    pass
+
+
+def my_main():
+    my_print("==unit=="*10)
+    my_unit_test()
+    my_print("==func==" * 10)
+    my_func_test()
+
+
+my_main()
