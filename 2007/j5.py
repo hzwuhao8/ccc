a36bd556a0c853572dad119355f58b92468afe4b
wuhao <wuhao@bokee.net>
2019-01-02 20:32:28 +0800

    j5 init

---
+++ 2007/j5.py
@@ -0,0 +1,34 @@
+#
+
+
+def my_print(x, end="\n"):
+    print(x, end=end)
+    pass
+
+
+def my_input():
+    pass
+
+
+def my_run(data):
+
+    pass
+
+
+def my_unit_test():
+    pass
+
+
+def my_func_test():
+
+    pass
+
+
+def my_main():
+    my_print("==unit=="*10)
+    my_unit_test()
+    my_print("==func==" * 10)
+    my_func_test()
+
+
+my_main()
