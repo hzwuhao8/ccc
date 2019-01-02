d189f3c07120f22d51cae1eab0453102805afee1
wuhao <wuhao@bokee.net>
2019-01-02 19:30:00 +0800

    模版

---
+++ 2007/j0.py
@@ -0,0 +1,32 @@
+#
+
+
+def my_print(x, end="\n"):
+    print(x, end=end)
+    pass
+
+
+def my_input():
+    pass
+
+
+def my_run():
+    pass
+
+
+def my_unit_test():
+    pass
+
+
+def my_func_test():
+    pass
+
+
+def my_main():
+    my_print("==unit=="*10)
+    my_unit_test()
+    my_print("==func==" * 10)
+    my_func_test()
+
+
+my_main()
