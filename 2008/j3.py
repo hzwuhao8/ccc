0c152aacc12c7af82757a7b518895e606826271f
wuhao <wuhao@bokee.net>
2019-01-03 22:22:44 +0800

    write  unit test

--- 2008/j3.py
+++ 2008/j3.py
@@ -7,25 +7,37 @@ def my_print(x, end="\n"):
 
 
 def my_input():
+    input_data = input()
+    return input_data
     pass
 
 
-def my_run(data):
+def my_move(form, to):
+    steps = 0
+    return steps
 
+
+def my_run(data):
     pass
 
 
 def my_unit_test():
+    assert my_move('A', 'A') == 0
+    assert my_move('A', 'G') == 1
+    assert my_move('G', 'P') == 4
+    assert my_move('P', 'S') == 4
+    assert my_move('S', 'enter') == 6
     pass
 
 
 def my_func_test():
-
+    assert my_run("GPS") == 15
+    assert my_run("ECHO ROCK") == 29
     pass
 
 
 def my_main_test():
-    my_print("==unit=="*10)
+    my_print("==unit==" * 10)
     my_unit_test()
     my_print("==func==" * 10)
     my_func_test()
@@ -42,4 +54,3 @@ my_main_test()
 quit()
 
 my_main()
-
