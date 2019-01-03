76ae6374e004717cee76dfdc36a5a01f291b3882
wuhao <wuhao@bokee.net>
2019-01-03 22:38:13 +0800

    j3 完成 比较 顺利

--- 2008/j3.py
+++ 2008/j3.py
@@ -2,7 +2,7 @@
 
 
 def my_print(x, end="\n"):
-    print(x, end=end)
+    # print(x, end=end)
     pass
 
 
@@ -16,34 +16,57 @@ base_data = [list('ABCDEF'),
              list('GHIJKL'),
              list('MNOPQR'),
              list('STUVWX'),
-             list('YZ - . \n'),
+             list('YZ -.\n'),
              ]
 
 
-def my_move(form, to):
-    steps = 0
+def get_x_y(c):
+    for i in range(5):
+        for j in range(6):
+            if base_data[i][j] == c:
+                return (i, j)
+            else:
+                continue
+    print("c={0} ERROR".format(c))
 
+
+def my_move(f, to):
+
+    (x1, y1) = get_x_y(f)
+    (x2, y2) = get_x_y(to)
+    steps = abs(x1 - x2) + abs(y1 - y2)
     return steps
 
 
 def my_run(data):
+    total = 0
+    f = 'A'
+    data_list = list(data)
+    data_list.append("\n")
+    for t in data_list:
+        steps = my_move(f, t)
+        my_print("f={0} t={1} steps={2}".format(f, t, steps))
+        f = t
+        total += steps
+    return total
     pass
 
 
 def my_unit_test():
     assert len(base_data) == 5
-    assert len(base_data[2]) == 5
+    assert len(base_data[2]) == 6
+    assert "".join(sum(base_data, [])) == "ABCDEFGHIJKLMNOPQRSTUVWXYZ -.\n"
     assert base_data[3][2] == 'U'
     assert my_move('A', 'A') == 0
     assert my_move('A', 'G') == 1
     assert my_move('G', 'P') == 4
     assert my_move('P', 'S') == 4
-    assert my_move('S', 'enter') == 6
+    assert my_move('S', '\n') == 6
     pass
 
 
 def my_func_test():
-    assert my_run("GPS") == 15
+    assert my_run("GPS") == 15 , my_run("GPS")
     assert my_run("ECHO ROCK") == 29
     pass
 
@@ -63,6 +86,6 @@ def my_main():
 
 my_main_test()
 
-quit()
+#quit()
 
 my_main()
