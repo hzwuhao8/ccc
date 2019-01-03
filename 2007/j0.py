400a29e585c551c182040d2a4b4a69fd73b25c0c
wuhao <wuhao@bokee.net>
2019-01-03 21:33:38 +0800

    add  my_main_test

--- 2007/j0.py
+++ 2007/j0.py
@@ -24,11 +24,22 @@ def my_func_test():
     pass
 
 
-def my_main():
+def my_main_test():
     my_print("==unit=="*10)
     my_unit_test()
     my_print("==func==" * 10)
     my_func_test()
 
 
+def my_main():
+    input_data = my_input()
+    my_res = my_run(input_data)
+    print(my_res)
+
+
+my_main_test()
+
+quit()
+
 my_main()
+
