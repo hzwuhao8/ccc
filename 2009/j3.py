426c4c6a8c6199c48c05bcd0a6071a70a16c8fbf
wuhao <wuhao@bokee.net>
2019-01-06 21:21:43 +0800

    这题目 还简单的 18分钟 输出化了点时间

--- 2009/j3.py
+++ 2009/j3.py
@@ -20,11 +20,26 @@ def my_print_trace(x, end="\n"):
 
 
 def my_input():
-    pass
+    my_time = int(input())
+    return my_time
 
 
 def my_run(data):
-    pass
+    tz = [0, -3, -2, -1, 0, 1]
+    if data <= 59:
+        h, m = 0, data
+    else:
+        h = data // 100
+        m = data % 100
+
+    res = [(h + x) % 24 * 100 + m for x in tz]
+    if m >= 30:
+        last = (h + 1 + 1) % 24 * 100 + (m + 30) % 60
+    else:
+        last = (h + 1) % 24 * 100 + (m + 30)
+    res.append(last)
+    my_print(res)
+    return res
 
 
 def my_unit_test():
@@ -32,6 +47,7 @@ def my_unit_test():
 
 
 def my_func_test():
+    assert my_run(1300) == [1300, 1000, 1100, 1200, 1300, 1400, 1430]
     pass
 
 
@@ -45,7 +61,14 @@ def my_main_test():
 def my_main():
     input_data = my_input()
     my_res = my_run(input_data)
-    print(my_res)
+
+    print("""{ll[0]} in Ottawa.
+{ll[1]} in Victoria.
+{ll[2]} in Edmonton.
+{ll[3]} in Winnipeg.
+{ll[4]} in Toronto.
+{ll[5]} in Halifax.
+{ll[6]} in St. John's.""".format(ll=my_res))
 
 
 if os.environ.get("UNIT", None):
