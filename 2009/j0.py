13405d7fb5d22805e61b13f61111396641a70f40
wuhao <wuhao@bokee.net>
2019-01-06 20:15:31 +0800

    2009 开发环境准备

---
+++ 2009/j0.py
@@ -0,0 +1,58 @@
+#
+import os
+import sys
+import copy
+import time
+
+
+def my_print(x, end="\n"):
+    if os.environ.get('DEBUG', None) or os.environ.get('TRACE', None):
+        print(x, file=sys.stderr, end=end)
+    else:
+        pass
+
+
+def my_print_trace(x, end="\n"):
+    if os.environ.get('TRACE', None):
+        print(x, file=sys.stderr, end=end)
+    else:
+        pass
+
+
+def my_input():
+    pass
+
+
+def my_run(data):
+    pass
+
+
+def my_unit_test():
+    pass
+
+
+def my_func_test():
+    pass
+
+
+def my_main_test():
+    my_print("==unit==" * 10)
+    my_unit_test()
+    my_print("==func==" * 10)
+    my_func_test()
+
+
+def my_main():
+    input_data = my_input()
+    my_res = my_run(input_data)
+    print(my_res)
+
+
+if os.environ.get("UNIT", None):
+    my_unit_test()
+
+if os.environ.get("FUNC", None):
+    my_func_test()
+
+
+my_main()
