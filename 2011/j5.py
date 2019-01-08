20d576f56b8417fb05c2f10b3705ea652eef17e5
wuhao <hzwuhao8@gmail.com>
2019-01-08 15:47:15 +0800

    基本的 工具类 完成了 　

--- 2011/j5.py
+++ 2011/j5.py
@@ -26,6 +26,17 @@ class Tree:
         self.sub_tree.append(t)
         return t
 
+    def get_all_sub_node(self):
+        tmp1 = [x.v for x in self.sub_tree]
+        my_print("tmp1={0}".format(tmp1))
+        tmp2 = []
+        for t in self.sub_tree:
+            res = t.get_all_sub_node()
+            my_print("res={0}".format(res))
+            tmp2 = tmp2 + res
+        my_print("tmp2={0}".format(tmp2))
+        return (tmp1 + tmp2)
+
 
 def my_unit_test():
     n = 4
@@ -42,6 +53,14 @@ def my_unit_test():
     my_print("root={0}".format(root))
     for i in range(1, n + 1):
         my_print("a[{0}]={1}".format(i, a[i]))
+    my_print(a[4].get_all_sub_node())
+    dic = {}
+    for i in range(1, n + 1):
+        dic[i] = a[i].get_all_sub_node()
+    my_print(dic)
+    assert dic[1] == []
+    assert dic[3] == [1]
+    assert set(dic[4]) == set([1, 2, 3])
 
 
 my_unit_test()
