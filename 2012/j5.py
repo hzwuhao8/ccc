da13ec9189408fe89941c6157239539e1d886ca6
wuhao <wuhao@bokee.net>
2019-01-08 20:47:28 +0800

    开始写 辅助 函数

---
+++ 2012/j5.py
@@ -0,0 +1,18 @@
+# 广度优先
+
+
+def my_print(x, end="\n"):
+    print(x, end=end)
+    pass
+
+
+def my_run(n, init_list):
+    pass
+
+
+def my_func_test():
+    assert my_run(3, [3, 2, 1]) == 20
+    assert my_run(2, [2, 1]) == -1
+
+
+my_func_test()
