df5bd6613317bb387534ec8e452497d32790ac89
wuhao <hzwuhao8@gmail.com>
2019-01-08 11:34:47 +0800

    simple 8分钟

--- 2011/j3.py
+++ 2011/j3.py
@@ -2,13 +2,30 @@ def my_print(x, end="\n"):
     print(x, end=end)
 
 
-def my_run():
-    pass
+def my_run(t1, t2):
+    n = 2
+    while True:
+        t3 = t1 - t2
+        if t3 < 0:
+            break
+        else:
+            t1 = t2
+            t2 = t3
+            n = n + 1
+    return n
 
 
 def my_func_test():
-    pass
+    assert my_run(120, 71) == 5
 
 
 def my_main():
-    pass
+    t1 = int(input())
+    t2 = int(input())
+    res = my_run(t1, t2)
+    print(res)
+
+
+my_func_test()
+
+my_main()
