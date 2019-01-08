506a0e66d49656bffb32c54e2e0866a04f37b9e2
wuhao <hzwuhao8@gmail.com>
2019-01-08 14:26:12 +0800

    j2 init

--- 2011/j4.py
+++ 2011/j4.py
@@ -2,12 +2,15 @@ def my_print(x, end="\n"):
     print(x, end=end)
 
 
-def my_run(position_list, cmd,length):
+def my_run(position_list, cmd, length):
     pass
 
 
-def my_run(position_list,cmd_str):
-    pass
+def my_run(position_list, cmd_str):
+    cmd, length_str = cmd_str.split()
+    my_print("cmd_str={0} cmd={1} length_str={2}".format(cmd_str, cmd, length_str))
+    res_list = my_run(position_list, cmd, int(length_str))
+    return " ".join([str(x) for x in res_list])
 
 
 def my_func_test():
@@ -23,4 +26,3 @@ def my_func_test():
 
 
 my_func_test()
-
