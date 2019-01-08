d758e73d25f87a69471c1a0b54ca7838afdced79
wuhao <wuhao@bokee.net>
2019-01-08 19:53:01 +0800

    主要功能完成

---
+++ 2012/j2.py
@@ -0,0 +1,18 @@
+def my_func_test():
+    assert my_run([30, 10, 20, 20]) == "No Fish"
+    assert my_run([1, 10, 12, 13]) == "Fish Rising"
+
+
+def my_run(data):
+    if data.count(data[0]) == 4:
+        return "Fish At Constant Depth"
+    ll = sorted(data)
+    if ll == data:
+        return "Fish Rising"
+    elif ll == data.reverse():
+        return "Fish Diving"
+    else:
+        return "No Fish"
+
+
+my_func_test()
