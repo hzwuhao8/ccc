79a72d5ebf188914a972eb3d9e2700b96150f5ce
wuhao <wuhao@bokee.net>
2019-01-09 20:01:28 +0800

    func test pass

---
+++ 2014/j5.py
@@ -0,0 +1,28 @@
+def my_print(x):
+    print(x)
+    pass
+
+
+def my_func_test():
+    assert my_run(4, "Ada Alan Grace John", "John Grace Alan Ada") == "good"
+    assert my_run(7, "Rich Graeme Michelle Sandy Vlado Ron Jacob",
+                  "Ron Vlado Sandy Michelle Rich Graeme Jacob") == "bad"
+
+
+def my_run(total, str_a, str_b):
+    list_a = str_a.split()
+    list_b = str_b.split()
+    if len(list_a) == total and len(list_b) == total:
+        zip_list = list(zip(list_a, list_b))
+        my_print(zip_list)
+        for a, b in zip_list:
+            if (b, a) not in zip_list:
+                return "bad"
+        return "good"
+    else:
+        return "bad"
+
+    pass
+
+
+my_func_test()
