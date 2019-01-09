3552c70e7ba65a72f192083f1b041d6e1ed27de2
wuhao <hzwuhao8@gmail.com>
2019-01-09 15:07:12 +0800

    func test fixure

---
+++ 2013/j5.py
@@ -0,0 +1,16 @@
+# 计分问题， 如何赢
+
+
+def my_func_test():
+    data = [[1, 3, 7, 5], [3, 4, 0, 8], [2, 4, 2, 2]]
+    assert my_run(3, data) == 0
+
+    data = [[1, 3, 5, 7], [3, 4, 8, 0], [2, 4, 2, 2], [1, 2, 5, 5]]
+    assert my_run(3, data) == 9
+
+
+def my_run(team, data):
+    pass
+
+
+my_func_test()
