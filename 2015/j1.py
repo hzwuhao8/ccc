d113257c178b113ba17ce15d5ceabefafa62fcd6
wuhao <hzwuhao8@gmail.com>
2019-01-10 09:40:31 +0800

    func test pass

---
+++ 2015/j1.py
@@ -0,0 +1,18 @@
+def my_func_test():
+    assert my_run(1, 7) == "Before"
+    assert my_run(8, 31) == "After"
+    assert my_run(2, 18) == "Special"
+
+
+def my_run(m, d):
+    if m <= 1:
+        return "Before"
+    elif m == 2 and d < 18:
+        return "Before"
+    elif m == 2 and d == 18:
+        return "Special"
+    else:
+        return "After"
+
+
+my_func_test()
