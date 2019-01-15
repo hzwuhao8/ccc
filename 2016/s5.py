443aa09f6ef2e7f50cbca7da518b97917cbce2b6
wuhao <hzwuhao8@gmail.com>
2019-01-15 15:37:43 +0800

    func test pass ; input test pass

--- 2016/s5.py
+++ 2016/s5.py
@@ -6,7 +6,7 @@
 
 
 def my_print(x):
-    print(x)
+    # print(x)
     pass
 
 
@@ -18,6 +18,41 @@ def my_func_test():
 def my_run(n, t, my_str):
     data = [int(x) for x in my_str]
     my_print(data)
+    for i in range(t):
+        data = change(n, data)
+        my_print(data)
+
+    return "".join([str(x) for x in data])
+
+
+def change(n, data):
+    # 取 附近
+    data_result = data.copy()
+
+    for i in range(n):
+        pre = i - 1
+        suf = i + 1
+        if suf == n:
+            suf = 0
+        c0 = data[pre]
+        c1 = data[suf]
+        if c0 + c1 == 1:
+            data_result[i] = 1
+        else:
+            data_result[i] = 0
+    return data_result
 
 
 my_func_test()
+
+
+def my_main():
+    n, t = [int(x) for x in input().split()]
+    my_str = input()
+    res = my_run(n, t, my_str)
+    print(res)
+
+
+my_main()
+
+
