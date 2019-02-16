9ffe6cd0379fc737903dcf5d274746b176bb4ff2
wuhao <wuhao@bokee.net>
2019-02-16 18:47:43 +0800

    graph api 完成

--- algs4/graph.py
+++ algs4/graph.py
@@ -6,6 +6,13 @@ class Graph:
             self.dic[i] = [].copy()
         pass
 
+    def __str__(self):
+        tmp = []
+        for k, v in self.dic.items():
+            s = "{0} -> {1}".format(k, v)
+            tmp.append(s)
+        return "\n".join(tmp)
+
     def add_edge(self, v, w):
         l1 = self.dic.get(v, [])
         l1.append(w)
@@ -14,7 +21,6 @@ class Graph:
         l2 = self.dic.get(w, [])
         l2.append(v)
         self.dic[w] = l2
-        pass
 
     def v_size(self):
         return len(self.dic)
@@ -25,12 +31,17 @@ class Graph:
     def adj(self, v):
         return self.dic.get(v, [])
 
-    def __str__(self):
-        tmp = []
-        for k, v in self.dic.items():
-            s = "{0} -> {1}".format(k, v)
-            tmp.append(s)
-        return "\n".join(tmp)
+    def degree(self, v):
+        return len(self.dic.get(v, []))
+
+    def max_degree(self):
+        return max([len(x) for x in self.dic.values()])
+
+    def average_degree(self):
+        return 2.0 * self.v_size() / self.e_size()
+
+    def number_of_self_loops(self):
+        return len([x for x in self.dic.items() if x[0] in x[1]])
 
 
 def my_print(x, end="\n"):
@@ -38,13 +49,29 @@ def my_print(x, end="\n"):
 
 
 def my_unit_test():
-    v_size = 4
+    v_size = 13
     g1 = Graph(v_size)
-    g1.add_edge(1, 2)
-    g1.add_edge(1, 3)
+    g1.add_edge(0, 5)
+    g1.add_edge(4, 3)
+    g1.add_edge(0, 1)
+    g1.add_edge(9, 12)
+    g1.add_edge(6, 4)
+    g1.add_edge(5, 4)
+    g1.add_edge(0, 2)
+    g1.add_edge(11, 12)
+    g1.add_edge(9, 10)
+    g1.add_edge(0, 6)
+    g1.add_edge(7, 8)
+    g1.add_edge(9, 11)
+    g1.add_edge(5, 3)
+    g1.add_edge(13, 13)
+
     my_print(g1)
-    assert v_size == g1.v_size()
-    assert 2 == g1.e_size()
+    assert 14 == g1.v_size()
+    assert 14 == g1.e_size()
+    assert 4 == g1.degree(0)
+    assert 4 == g1.max_degree()
+    assert 1 == g1.number_of_self_loops()
 
 
 my_unit_test()
