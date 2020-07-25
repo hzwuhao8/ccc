c2e8fe1e051f01afe5f0ce19d0270d23374564a3
wuhao <wuhao@bokee.net>
2020-07-25 22:31:40 +0800

    md5 后，内存不超了 但是 超时了

--- 2020/s03.py
+++ 2020/s03.py
@@ -1,9 +1,15 @@
 import itertools
 import collections
+import hashlib
 
 n = input().strip()
 h = input().strip()
 
+# print(len(n))
+# print(len(h))
+
+n_size = len(n)
+h_size = len(h)
 #  1 生成搜索序列，  搜索序列 如何生成？  生成的 时候，如果  子列   不是 h 的 子列， 那么 这个排序 就不是 h的 子列
 #  用这种方法，可以 过滤 很大一部分  如何用程序表达 描述？
 #  n -> dict {"a" : 2 , "b": 1 } , 然后进行排列？  如何生成排列？ 如何 得到 子排列， 如何 过滤？和  树 有关系吗？
@@ -16,13 +22,24 @@ x = collections.Counter(n)
 # print(x)
 y = collections.Counter(h)
 str_set = set()
-for i in range(len(h) - len(n)+1):
-    ya = h[i:i + len(n)]
-    # print(ya)
-    t = collections.Counter(ya)
-    if t == x:
-        # print(ya)
-        str_set.add(ya)
+ya = h[0:n_size]
+
+# print(ya)
+t0 = collections.Counter(ya)
+# print(t0)
+if t0 == x:
+    str_set.add(hashlib.md5(ya.encode()).hexdigest())
+
+for i in range(1, h_size - n_size + 1):
+    if h[i - 1] != h[i + n_size - 1]:
+        t0.subtract(h[i - 1])
+        if t0[h[i - 1]] == 0:
+            del t0[h[i - 1]]
+        t0.update(h[i + n_size - 1])
+    # print(t0)
+    if t0 == x:
+        ya = h[i:i + n_size]
+        str_set.add(hashlib.md5(ya.encode()).hexdigest())
         count = count + 1
 
 print(len(str_set))
